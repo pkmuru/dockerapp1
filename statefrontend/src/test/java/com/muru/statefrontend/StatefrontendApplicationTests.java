0b91a92a3215cf1013bbc0694e071dd8d80d7f84
Muruganantham Padikkasu <pkmuru@gmail.com>
2019-10-09 22:50:30 +0100

    INIT

---
+++ statefrontend/src/test/java/com/muru/statefrontend/StatefrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.muru.statefrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatefrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
